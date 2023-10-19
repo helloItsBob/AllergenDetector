391cbb7241aa687906b68334fa11d6e9b61d71cb
Aleksandrs Bistrovs <abistrovs3@gmail.com>
2023-10-19 19:30:03 +0200

    Basic navigation functionality (#1)
    * Basic navigation functionality

--- app/src/main/java/com/bpr/allergendetector/ui/lists/ListsFragment.kt
+++ app/src/main/java/com/bpr/allergendetector/ui/lists/ListsFragment.kt
@@ -1,4 +1,4 @@
-package com.bpr.allergendetector.ui.home
+package com.bpr.allergendetector.ui.lists
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.bpr.allergendetector.databinding.FragmentHomeBinding
+import com.bpr.allergendetector.databinding.FragmentListsBinding
 
-class HomeFragment : Fragment() {
+class ListsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentListsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val listsViewModel =
+            ViewModelProvider(this).get(ListsViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentListsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textLists
+        listsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
