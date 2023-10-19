133d54971454c8b18312c0d16d09845674f2842c
helloItsBob <abistrovs3@gmail.com>
2023-10-19 19:16:35 +0200

    Remove unused imports

--- app/src/main/java/com/bpr/allergendetector/ui/lists/ListsFragment.kt
+++ app/src/main/java/com/bpr/allergendetector/ui/lists/ListsFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SeekBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.bpr.allergendetector.R
 import com.bpr.allergendetector.databinding.FragmentListsBinding
 
 class ListsFragment : Fragment() {
